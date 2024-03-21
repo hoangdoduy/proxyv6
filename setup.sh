4e559c160618689ade767bbac5a46fb8a70ea93a
hoangdoduy <doduyhoang516@gmail.com>
2024-03-21 14:40:26 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -47,9 +47,11 @@ setuid 65535
 stacksize 6291456 
 pidfile /usr/local/etc/3proxy/3proxy.pid
 flush
+auth strong
+
+users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
 
 $(awk -F "/" '{print "auth strong\n" \
-"users " $1 ":CL:" $2 "\n" \
 "allow " $1 "\n" \
 "proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
 "flush\n"}' ${WORKDATA})
